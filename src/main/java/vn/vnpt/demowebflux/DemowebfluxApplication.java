b90ef2e805dcf375a0bfea448009c6fa2e1963c1
tanlpt <tanlpt@vnpt.vn>
2021-01-06 16:36:14 +0700

    first commit

---
+++ src/main/java/vn/vnpt/demowebflux/DemowebfluxApplication.java
@@ -0,0 +1,13 @@
+package vn.vnpt.demowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/vnpt/demowebflux/DemowebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.vnpt.demowebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
